e3e7fa80bf1652b70e2231b1142e9245464dfd12
Jake Marchewitz <jjmarchewitz@gmail.com>
2023-10-06 18:18:25 -0400

    initial commit

---
+++ src/eye_am_a_feedback_loop/util.py
@@ -0,0 +1,16 @@
+import os
+from pathlib import Path
+
+_repo_name = "Digital_Audio"
+
+def get_repo_root():
+    return _find_repo_root(Path(__file__))
+
+def _find_repo_root(path: Path):
+    if os.path.basename(path) == "":
+        raise FileExistsError(f"Unable to find the root of the repository. Expected to find \"{_repo_name}\"")
+
+    if os.path.basename(path) == _repo_name:
+        return path
+    else:
+        return _find_repo_root(path.parent)
